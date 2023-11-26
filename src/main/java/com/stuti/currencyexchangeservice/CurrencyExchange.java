e123e1ea02554d048205f6bc6d3fe36df8d83a75
megha <stutimegha2019@gmail.com>
2023-11-26 11:46:12 +0530

    adding jpa and h2 to connect database

--- src/main/java/com/stuti/currencyexchangeservice/CurrencyExchange.java
+++ src/main/java/com/stuti/currencyexchangeservice/CurrencyExchange.java
@@ -1,12 +1,22 @@
 package com.stuti.currencyexchangeservice;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
+    private String environment;
 
     public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
         this.id = id;
@@ -48,5 +58,13 @@ public class CurrencyExchange {
 
     public void setConversionMultiple(BigDecimal conversionMultiple) {
         this.conversionMultiple = conversionMultiple;
+    }
+
+    public String getEnvironment(){
+        return environment;
+    }
+
+    public void setEnvironment(String environment){
+        this.environment=environment;
     }
 }
--- src/main/java/com/stuti/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/stuti/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.stuti.currencyexchangeservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +11,15 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @Autowired
+    private Environment environment;
 
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
-        return new CurrencyExchange(1L ,from,to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = new CurrencyExchange(1L ,from,to, BigDecimal.valueOf(82));
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
     }
 
 }
