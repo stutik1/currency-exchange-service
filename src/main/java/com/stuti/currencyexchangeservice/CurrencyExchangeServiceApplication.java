7a6cd1297ef78e1e8a23772d8e8af5607160c806
megha <stutimegha2019@gmail.com>
2023-11-27 15:08:23 +0530

    adding eureka

--- src/main/java/com/stuti/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/stuti/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,8 +2,9 @@ package com.stuti.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+@EnableEurekaServer
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
 
